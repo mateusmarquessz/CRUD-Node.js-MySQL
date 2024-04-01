5437e0a8e12f285c093d40f1603cd4e76c07d954
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-04-01 00:07:49 -0300

    Criando a base do Site

---
+++ front-end/src/App.js
@@ -0,0 +1,37 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./Components/Form.js";
+import {toast, ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+
+const Title = styled.h2``;
+
+
+
+function App() {
+  return (
+    <>
+    <Container>
+      <Title>USUARIOS</Title>
+      <Form/>
+    </Container>
+   
+    <GlobalStyle/>
+    </>
+  );
+  
+}
+
+export default App;
