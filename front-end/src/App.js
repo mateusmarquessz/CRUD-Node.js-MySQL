b9b58985a2bfb9c045fde462d697be0bc6c324a1
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-04-01 12:51:56 -0300

    Update App.js
    Dando update na tela inicial, no caso ativando o metodo getUsuarios do Site

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,12 @@
 import GlobalStyle from "./styles/global";
 import styled from "styled-components";
 import Form from "./Components/Form.js";
+import Grid from "./Components/Grid.js";
+import { useState } from "react";
+import { useEffect } from "react";
 import {toast, ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
+import axios from "axios";
 
 
 const Container = styled.div`
@@ -21,11 +25,28 @@ const Title = styled.h2``;
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () =>{
+    try{
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a,b) => (a.nome>b.nome ? 1 : -1)));
+    } catch(error){
+      toast.error(error);
+    }
+  };
+
+  useEffect(()=>{
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
     <Container>
       <Title>USUARIOS</Title>
       <Form/>
+      <Grid users ={users}/>
     </Container>
    
     <GlobalStyle/>
