0c0f0bff9ad92beb73849607b8b608bbdfc5920a
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-04-01 16:01:03 -0300

    Criando as funcoes de delete, update e  create

--- API/routes/uses.js
+++ API/routes/uses.js
@@ -1,8 +1,13 @@
 import express from "express";
-import {getUsers} from "../controllers/users.js";
+import {getUsers, addUser, updateUser, deleteUser} from "../controllers/users.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 
+router.get("/", addUser);
+
+router.get("/:id", updateUser);
+
+router.get("/:id", deleteUser);
 export default router
